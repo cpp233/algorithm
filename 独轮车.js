593a394b2c441752b721af9b6040153abf5a76e6
cpp233 <962582834@qq.com>
2020-05-24 22:59:23 +0800

    周末跟新

--- 独轮车.js
+++ 独轮车.js
@@ -10,77 +10,6 @@
 // @grant        none
 // ==/UserScript==
 
-//隐藏现有的dom
-function removeElement(){
-
-    document.getElementsByClassName("Horn4Category")[0].style.display = "none";         //全区喇叭
-    document.getElementsByClassName("ChatNobleBarrage")[0].style.display = "none";         //全区喇叭
-    document.getElementsByClassName("ChatSend-txt")[0].style.display = "none";         //
-    console.log("移除元素成功")
-}
-//添加自己的dom
-function addElement(){
-
-    //获取需要填加的父元素
-    var P = document.getElementsByClassName("ChatToolBar")[0];
-
-    // 添加按钮
-    if(document.getElementById("my_button") == null){
-        //添加按钮、设置按钮属性
-        var button = document.createElement("input");
-        button.type = "button";
-        button.value = "开";
-        button.type = "button"
-        button.id = "my_button";
-
-        P.appendChild(button);
-    }
-    //添加 小说模式&一般模式
-    if(document.getElementById("my_mod_article") == null){
-        var textLab1 = document.createElement("lable");
-        textLab1.innerText = " 小说";
-        P.appendChild(textLab1);
-
-        var checkbox1 = document.createElement("input");
-        checkbox1.type = "checkbox";
-        checkbox1.id = "my_mod_article";
-        P.appendChild(checkbox1);
-    }
-    // 添加 是否随机模式
-    if(document.getElementById("my_mod_random") == null){
-
-        var textLab2 = document.createElement("lable");
-        textLab2.innerText = " 随机";
-        P.appendChild(textLab2);
-
-        var checkbox2 = document.createElement("input");
-        checkbox2.type = "checkbox";
-        checkbox2.id = "my_mod_random";
-        P.appendChild(checkbox2);
-    }
-
-    //添加自定义文本框
-    var chat = document.getElementsByClassName("ChatSend")[0];
-    var node = document.getElementsByClassName("ChatBarrageCollect")[0];
-    if(document.getElementById("my_text") == null){
-
-        //添加按钮、设置按钮属性
-        var textDom = document.createElement("textarea");
-        textDom.id = "my_text";
-        textDom.style.width = "192px";
-        textDom.style.height = "37px";
-        textDom.style.float = "left";
-        textDom.style.padding = "5px 13px 5px 71px";
-        textDom.style.border = "1px solid ##00bcd4";
-        textDom.style.borderRight = "0";
-        textDom.placeholder = "自己的输入框";
-
-        chat.insertBefore(textDom, node);
-    }
-
-    console.log("添加按钮成功");
-}
-
 //反复执行
 function loop(dom, str){
 
@@ -168,8 +97,6 @@ function init(){
 
         //document.title = document.hidden; // change tab text for demo
     })
-
-
     setTimeout(addElement, 4000);
     setTimeout(removeElement, 6000);
     setTimeout(bind, 6000);
@@ -202,6 +129,157 @@ function ex(){
 
 }
 
+var Unicycle = {
+    // 初始化
+    init : function(pDom){
+        myPDom = pDom;
+        // this.createDom()
+        this.addMoveEvent( this.createDom() );
+    },
+    // 创建一个悬浮的dom
+    createDom : function(){
+        var dom = document.createElement("div");
+        dom.id = "unicycleMainDiv";
+        var strCSS = 
+        `<style>
+        #unicycleMainDiv:hover {
+            box-shadow: 0 0 8px #0ca4d4;
+        }
+        #unicycleMainDiv {
+            width: 300px;
+            height: 50px;
+            display: flex;
+            z-index: 999;
+            position: absolute;
+            top: 4px;
+            right: 9px;
+            background: white;
+            border: 1px solid #cccccc;
+            border-radius: 4px;
+        }
+        </style>
+        <div id = "myDiv1"></div>
+        <div id = "myDiv2"></div>`;
+        dom.innerHTML = strCSS;     // 此处用 innerText 会出BUG
+        myPDom.appendChild(dom);
+        return dom;
+    },
+    // 给特定的DOM添加可以被拖动事件
+    addMoveEvent : function(parDom){
+        //添加自由拖动事件(只能在父窗口区域内移动)
+        var dom = parDom;
+        dom.onmousedown = function(event) {
+            var ev = event || window.event;
+            event.stopPropagation();
+            var disX = ev.clientX - dom.offsetLeft;
+            var disY = ev.clientY - dom.offsetTop;
+            var isMove = true;
+            document.onmousemove = function(event){
+                dom.style.cursor = "move";
+                var ev = event || window.event;
+                event.stopPropagation();
+                var x = ev.clientX - disX;
+                var y = ev.clientY - disY;
+                if( (x > 0) &&
+                    // (x < window.innerWidth - dom.clientWidth) &&
+                    (x < dom.parentNode.offsetWidth - dom.clientWidth) &&
+                    isMove ){
+                    dom.style.left = x + "px";
+                }
+                if( (y > 0) &&
+                    // (y < window.innerHeight - dom.clientHeight) &&
+                    (y < dom.parentNode.offsetHeight - dom.clientWidth) &&
+                    isMove ){
+                    dom.style.top = y + "px";
+                }
+                // console.log(
+                //     (x > 0),
+                //     (x < window.innerWidth - dom.clientWidth),
+                //     (y > 0),
+                //     (y < window.innerHeight - dom.clientHeight)
+                // );
+            };
+            dom.onmouseup = function() {
+                document.onmousemove = null;
+                disY = 0;
+                disX = 0;
+                isMove = false;
+                dom.style.cursor = "";
+            };
+            dom.onmouseleave = function(){
+                // dom.onmousemove = null;
+                // isMove = false;
+                // dom.onmousedown = null;
+            };
+        }; 
+    },
+    addElement : function (){
+        // 添加按钮
+        //添加按钮、设置按钮属性
+        var button = document.createElement("input");
+        button.type = "button";
+        button.value = "开";
+        button.type = "button"
+        button.id = "my_button";
+        P.appendChild(button);
 
+        //添加 小说模式&一般模式
+        var textLab1 = document.createElement("lable");
+        textLab1.innerText = " 小说";
+        P.appendChild(textLab1);
+        var checkbox1 = document.createElement("input");
+        checkbox1.type = "checkbox";
+        checkbox1.id = "my_mod_article";
+        P.appendChild(checkbox1);
 
+        // 添加 是否随机模式
+        var textLab2 = document.createElement("lable");
+        textLab2.innerText = " 随机";
+        P.appendChild(textLab2);
+        var checkbox2 = document.createElement("input");
+        checkbox2.type = "checkbox";
+        checkbox2.id = "my_mod_random";
+        P.appendChild(checkbox2);
 
+        //添加自定义文本框
+        var chat = document.getElementsByClassName("ChatSend")[0];
+        var node = document.getElementsByClassName("ChatBarrageCollect")[0];
+
+        //添加按钮、设置按钮属性
+        var textDom = document.createElement("textarea");
+        textDom.id = "my_text";
+        textDom.style.width = "192px";
+        textDom.style.height = "37px";
+        textDom.style.float = "left";
+        textDom.style.padding = "5px 13px 5px 71px";
+        textDom.style.border = "1px solid ##00bcd4";
+        textDom.style.borderRight = "0";
+        textDom.placeholder = "自己的输入框";
+
+        chat.insertBefore(textDom, node);
+    },
+    myPDom : undefined,
+
+}
+Unicycle.init(document.getElementsByTagName("body")[0]);
+
+// var dragdiv = document.querySelector('#unicycleMainDiv');
+// var x, y;  //记录到点击时鼠标到移动框左边和上边的距离
+
+// dragdiv.addEventListener('dragstart', function (e) {
+//     e.dataTransfer.effectAllowed = "move";  //移动效果
+//     e.dataTransfer.setData("text", '');  //附加数据，　没有这一项，firefox中无法移动
+//     x = e.offsetX || e.layerX;
+//     y = e.offsetY || e.layerY;
+//     return true;
+// }, false);
+
+// document.addEventListener('dragover', function (e) {//取消冒泡 ,不取消则不能触发 drop事件
+//     e.preventDefault()|| e.stopPropagation();
+// }, false);
+
+// document.addEventListener('drop', function (e) {
+//     dragdiv.style.left = (e.pageX - x) + 'px';
+//     dragdiv.style.top = (e.pageY - y) + 'px';
+//     e.preventDefault() || e.stopPropagation();  //不取消，firefox中会触发网页跳转到查找setData中的内容
+// }, false);
